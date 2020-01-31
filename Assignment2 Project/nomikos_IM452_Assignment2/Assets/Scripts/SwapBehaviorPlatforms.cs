18a0cbfeac9bd0e01b1079dc83ce5a6a00cd7799
probopastor <43503592+probopastor@users.noreply.github.com>
2020-01-31 04:36:43 -0600

    Swap Behavior Platforms

---
+++ Assignment2 Project/nomikos_IM452_Assignment2/Assets/Scripts/SwapBehaviorPlatforms.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapBehaviorPlatforms : Platforms
+{
+    private bool doOnce; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doOnce = false;
+        SetMovementType(gameObject.AddComponent(typeof(NoMovementBehavior)) as IMovementType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            if (!doOnce)
+            {
+                doOnce = true;
+                Platforms.switchPlatformBehaviors++;
+            }
+        }
+        
+    }
+}
