a9edaf209de30c82e13a20fe441d649f5507d60b
probopastor <43503592+probopastor@users.noreply.github.com>
2020-01-30 22:44:41 -0600

    Player movement

--- Assignment2 Project/nomikos_IM452_Assignment2/Assets/PlayerBehavior.cs
+++ Assignment2 Project/nomikos_IM452_Assignment2/Assets/PlayerBehavior.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    private Rigidbody2D playerRb;
+
+    [SerializeField] private float moveSpeed = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float movePlayer = Input.GetAxis("Horizontal");
+
+        playerRb.velocity = new Vector2(moveSpeed * movePlayer, playerRb.velocity.y);
     }
 }
