ffa5f199fa617a61e39a8309b3999f60cfb7d134
probopastor <43503592+probopastor@users.noreply.github.com>
2020-01-31 16:26:41 -0600

    Pause manager + restart game

---
+++ Assignment2 Project/nomikos_IM452_Assignment2/Assets/PauseManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseManager : MonoBehaviour
+{
+    private bool paused;
+
+    public GameObject PauseCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+        PauseCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if(paused == false)
+        {
+            paused = true;
+            Time.timeScale = 0;
+            PauseCanvas.SetActive(true);
+        }
+        else if(paused == true)
+        {
+            Time.timeScale = 1;
+            paused = false;
+            PauseCanvas.SetActive(false);
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
