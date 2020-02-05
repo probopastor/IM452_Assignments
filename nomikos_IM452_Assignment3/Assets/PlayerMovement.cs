8ccde680c498677b194addd12f3cb527bbe1e615
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-04 21:42:26 -0600

    Made gravity stuff

--- nomikos_IM452_Assignment3/Assets/PlayerMovement.cs
+++ nomikos_IM452_Assignment3/Assets/PlayerMovement.cs
@@ -8,14 +8,39 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
 
+    public float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = gravity;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        controller.Move(velocity * Time.deltaTime);
+
+        Debug.Log(velocity.y);
+
     }
 }
