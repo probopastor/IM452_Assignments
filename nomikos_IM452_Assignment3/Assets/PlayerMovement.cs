33c8729d69c71f363b4f9ab0ac80b0c042cd0c44
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-04 22:08:28 -0600

    Jumping

--- nomikos_IM452_Assignment3/Assets/PlayerMovement.cs
+++ nomikos_IM452_Assignment3/Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float jumpHeight = 1f;
 
     public float gravity = -9.81f;
 
@@ -37,6 +38,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         controller.Move(velocity * Time.deltaTime);
 
