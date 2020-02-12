a2df10d4eeb1c5f78dc4cb154740d32e1268d9bb
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-12 17:37:51 -0600

    Movement stuff

---
+++ nomikos_IM452_Assignment4/Assets/MeteorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorController : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * movementSpeed, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.up * Time.deltaTime * movementSpeed, Space.World);
+        }
+    }
+}
