fcefb20a2e2f05806387ac1a55486cd5ac209296
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-12 18:19:23 -0600

    w

--- nomikos_IM452_Assignment4/Assets/MeteorController.cs
+++ nomikos_IM452_Assignment4/Assets/MeteorController.cs
@@ -6,16 +6,19 @@ public class MeteorController : MonoBehaviour
 {
     public float movementSpeed = 1f;
 
+    private Vector2 screenBounds; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        //Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.up * Time.deltaTime * movementSpeed, Space.World);
         }
@@ -23,5 +26,21 @@ public class MeteorController : MonoBehaviour
         {
             transform.Translate(-Vector3.up * Time.deltaTime * movementSpeed, Space.World);
         }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed, Space.World);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector3.right * Time.deltaTime * movementSpeed, Space.World);
+        }
+
+
+        
+        Vector3 playerPos = Camera.main.WorldToViewportPoint(transform.position);
+        playerPos.x = Mathf.Clamp01(playerPos.x);
+        playerPos.y = Mathf.Clamp01(playerPos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(playerPos);
     }
 }
