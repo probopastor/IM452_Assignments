1dede2f295395f05b29b0ed3a66c08b98effed09
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-14 17:17:59 -0600

    Health Text

---
+++ nomikos_IM452_Assignment4/Assets/PlayerHealthText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthText : MonoBehaviour
+{
+    public Text playerHealthText;
+
+    private float maxHealth = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = MeteorController.currentPlayerHealth;
+        playerHealthText.text = "Health: " + MeteorController.currentPlayerHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealthText.text = "Health: " + MeteorController.currentPlayerHealth;
+    }
+}
