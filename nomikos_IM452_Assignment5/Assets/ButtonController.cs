36108f3d52610e56c0a413ee9ae6ed113887d99d
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-21 16:36:20 -0600

    Button timer until next click

--- nomikos_IM452_Assignment5/Assets/ButtonController.cs
+++ nomikos_IM452_Assignment5/Assets/ButtonController.cs
@@ -7,6 +7,9 @@ public class ButtonController : MonoBehaviour
     public Color clickedColor;
     public Color defaultColor;
 
+    public int timeUntilButtonClick = 1;
+    private int timeRemaining = 0;
+
     private Renderer buttonRend;
     private bool isClicked;
 
@@ -16,12 +19,13 @@ public class ButtonController : MonoBehaviour
         isClicked = false;
         buttonRend = gameObject.GetComponent<Renderer>();
         buttonRend.material.color = defaultColor;
+        timeRemaining = timeUntilButtonClick;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(timeRemaining);
     }
 
     public void ClickButton()
@@ -30,11 +34,26 @@ public class ButtonController : MonoBehaviour
         {
             isClicked = true;
             buttonRend.material.color = clickedColor;
+
+            timeRemaining = timeUntilButtonClick;
+            StartCoroutine("ButtonTimer");
         }
-        else if(isClicked)
+    }
+
+    public IEnumerator ButtonTimer()
+    {
+        if(timeRemaining > 0)
         {
-            isClicked = false;
+            timeRemaining--;
+            yield return new WaitForSeconds(1);
+            StartCoroutine("ButtonTimer");
+        }
+        else
+        {
+            yield return new WaitForSeconds(1);
+            timeRemaining = timeUntilButtonClick;
             buttonRend.material.color = defaultColor;
+            isClicked = false;
         }
     }
 }
