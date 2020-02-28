53f8e90b423f5c341e4cd08db63f1c0dc22529a3
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-28 15:59:55 -0600

    Pause, Restart, and Exit to Main Menu functionality

---
+++ nomikos_IM452_Assignment6/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,89 @@
+﻿/*
+* William Nomikos
+* PauseManager.cs
+* Assignment 5
+* Handles pause in-game pause menu functionality and button functionality,
+* along with game music.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+
+    private bool paused;
+
+    public GameObject PauseCanvas;
+
+    //public AudioSource GameSource;
+    //public AudioClip gameMusic;
+
+    //public AudioSource SoundEffectSource;
+    //public AudioClip buttonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+
+        paused = false;
+        PauseCanvas.SetActive(false);
+
+        //GameSource.clip = gameMusic;
+        //GameSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (paused == false)
+        {
+            paused = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0;
+            PauseCanvas.SetActive(true);
+        }
+        else if (paused == true)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            Time.timeScale = 1;
+            paused = false;
+            PauseCanvas.SetActive(false);
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Confined;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Confined;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ButtonClick()
+    {
+        //SoundEffectSource.clip = buttonClick;
+        //SoundEffectSource.Play();
+    }
+
+
+}
