945ce8833b936cbc2f4502526988d3ff96819d16
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-28 20:56:56 -0600

    Main Menu, How to Play, and Music

---
+++ nomikos_IM452_Assignment6/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,44 @@
+﻿/*
+* William Nomikos
+* MainMenuManager.cs
+* Assignment 5
+* Handles main menu button functionality and sound effects.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public AudioSource GameSource;
+    public AudioClip gameMusic;
+
+    public AudioSource SoundEffectSource;
+    public AudioClip buttonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameSource.clip = gameMusic;
+        GameSource.Play();
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ButtonClick()
+    {
+        SoundEffectSource.clip = buttonClick;
+        SoundEffectSource.Play();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
