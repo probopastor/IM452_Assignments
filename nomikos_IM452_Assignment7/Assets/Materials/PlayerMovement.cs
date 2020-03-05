688fef88998c1120181b126a45e91053de202e31
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-05 06:16:08 -0600

    Player movement and Jumping

---
+++ nomikos_IM452_Assignment7/Assets/Materials/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿/*
+* William Nomikos
+* PlayerMovement.cs
+* Assignment 5
+* Script handles player behavior, including first person movement, jumping, 
+* player gravity, and player health.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float jumpHeight = 1f;
+
+    public float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = gravity;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
