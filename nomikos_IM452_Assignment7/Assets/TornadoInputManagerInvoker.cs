33778513505b0c26478414761d8d6593fab2b463
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-05 21:54:23 -0600

    Made invoker stack

--- nomikos_IM452_Assignment7/Assets/TornadoInputManagerInvoker.cs
+++ nomikos_IM452_Assignment7/Assets/TornadoInputManagerInvoker.cs
@@ -7,9 +7,14 @@ public class TornadoInputManagerInvoker : MonoBehaviour
     public PlayerScale playerScale;
     private ICommand changeSize;
 
+    Dictionary<string, ICommand> commands;
+    private Stack<ICommand> commandStack;
+
     // Start is called before the first frame update
     void Start()
     {
+        commandStack = new Stack<ICommand>();
+
         changeSize = new ChangeSize(playerScale);
     }
 
@@ -18,12 +23,61 @@ public class TornadoInputManagerInvoker : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.A))
         {
-            changeSize.Execute();
+            //changeSize.Execute();
+            AddCommand(changeSize);
+            InvokeCommand();
+
+            DisplayCommands();
         }
 
         if(Input.GetKeyDown(KeyCode.Q))
         {
-            changeSize.Undo();
+            //changeSize.Undo();
+            //AddCommand(changeSize);
+            InvokeUndoCommand();
+            commandStack.Pop();
+            DisplayCommands();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            //commandStack.Pop();
+            Debug.Log("Command popped: " + commandStack.Pop());
         }
     }
+
+    public void AddCommand(ICommand command)
+    {
+        commandStack.Push(command);
+    }
+
+    public void DisplayCommands()
+    {
+        Debug.Log("----- List of Commands in Remote Control -----\n");
+
+        foreach (ICommand command in commandStack)
+        {
+            Debug.Log("Command: " + command + "\n");
+        }
+    }
+
+    public void InvokeCommand()
+    {
+        commandStack.Peek().Execute();
+    }
+
+    public void InvokeUndoCommand()
+    {
+        if (commandStack.Count != 0)
+        {
+            commandStack.Peek().Undo();
+
+        }
+        else
+        {
+            Debug.Log("You tried to undo, but there are no more commands to undo.");
+        }
+    }
+
 }
+
