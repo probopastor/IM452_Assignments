880494a754162a3df090e30be114e1e722345fc6
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-06 18:07:45 -0600

    Era Changing Command

---
+++ nomikos_IM452_Assignment7/Assets/ChangeEra.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeEra : ICommand
+{
+    private EraHandler eraHandler;
+    Stack<int> eraHistory;
+
+    // Start is called before the first frame update
+    public ChangeEra(EraHandler era)
+    {
+        this.eraHandler = era;
+
+        eraHistory = new Stack<int>();
+    }
+
+    public void Execute()
+    {
+        eraHistory.Push(eraHandler.GetCurrentEra());
+        eraHandler.ChangeEra();
+    }
+
+    public void Undo()
+    {
+        if(eraHistory != null)
+        {
+            eraHandler.era = eraHistory.Pop();
+        }
+    }
+}
---
+++ nomikos_IM452_Assignment7/Assets/EraHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EraHandler : MonoBehaviour
+{
+    public GameObject[] objects;
+    public int timePerEra = 10;
+
+    public int era = 0;
+    public int maxEra = 2;
+
+    private int doOnceIndex = 0;
+    private bool doOnce = false;
+
+    private int counter = 0;
+    private TornadoInputManagerInvoker invoker;
+    private ICommand changeEra;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        invoker = new TornadoInputManagerInvoker();
+        changeEra = new ChangeEra(this);
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (i == era && !doOnce)
+            {
+                objects[i].SetActive(true);
+                doOnce = true;
+                doOnceIndex = i;
+            }
+            else if(i != doOnceIndex)
+            {
+                objects[i].SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        counter++;
+
+        if(counter >= timePerEra/0.02f)
+        {
+            invoker.AddCommand(changeEra);
+            invoker.InvokeCommand();
+
+            counter = 0;
+        }
+    }
+
+    public int GetCurrentEra()
+    {
+        return era;
+    }
+
+    public void ChangeEra()
+    {
+        era++;
+        if(era > maxEra)
+        {
+            era = maxEra;
+        }
+        else
+        {
+            doOnce = false;
+        }
+    }
+}
