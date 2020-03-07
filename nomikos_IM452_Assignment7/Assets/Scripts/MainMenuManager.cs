7fc506de3fdbe070af98d2cbdb025942bcc6ceb4
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-06 20:25:31 -0600

    Pause Menu

---
+++ nomikos_IM452_Assignment7/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,44 @@
+﻿/*
+* William Nomikos
+* MainMenuManager.cs
+* Assignment 6
+* Handles main menu functionality, including button functions, sound effects, and music.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public AudioSource GameSource;
+    public AudioClip gameMusic;
+
+    public AudioSource SoundEffectSource;
+    public AudioClip buttonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameSource.clip = gameMusic;
+        GameSource.Play();
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ButtonClick()
+    {
+        SoundEffectSource.clip = buttonClick;
+        SoundEffectSource.Play();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ nomikos_IM452_Assignment7/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,89 @@
+﻿/*
+* William Nomikos
+* PauseManager.cs
+* Assignment 6
+* Handles In-Game pause menu and functionality, including restarting, exiting
+* to main menu, button sound effects and game music.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+
+    private bool paused;
+
+    public GameObject PauseCanvas;
+
+    public AudioSource GameSource;
+    public AudioClip gameMusic;
+
+    public AudioSource SoundEffectSource;
+    public AudioClip buttonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+
+        paused = false;
+        PauseCanvas.SetActive(false);
+
+        GameSource.clip = gameMusic;
+        GameSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (paused == false)
+        {
+            paused = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0;
+            PauseCanvas.SetActive(true);
+        }
+        else if (paused == true)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            Time.timeScale = 1;
+            paused = false;
+            PauseCanvas.SetActive(false);
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Confined;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Confined;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ButtonClick()
+    {
+        SoundEffectSource.clip = buttonClick;
+        SoundEffectSource.Play();
+    }
+
+
+}
