9105c0998929429d1ef87f58cfadcb3ae9ae318a
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-25 17:22:04 -0500

    Background movement

---
+++ nomikos_IM452_Assignment8/Assets/BackgroundMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    public float backgroundSpeed = 0f;
+    public GameObject background;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-backgroundSpeed * Time.deltaTime, 0, 0);
+
+
+        if (transform.position.x <= -20.94)
+        {
+            float halfWidth = gameObject.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float otherHalfWidth = background.GetComponent<SpriteRenderer>().bounds.extents.x;
+
+            float xPos = background.transform.position.x + halfWidth + otherHalfWidth;
+
+            transform.position = new Vector3(xPos, background.transform.position.y, background.transform.position.z);
+        }
+    }
+}
