6166228d55800884b020bac5c3cd026ceb316b04
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-27 15:37:03 -0500

    Obstacle spawning

--- nomikos_IM452_Assignment8/Assets/ObstacleSpawning.cs
+++ nomikos_IM452_Assignment8/Assets/ObstacleSpawning.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ObstacleSpawning : MonoBehaviour
 {
+    public GameObject[] obstacleArray;
+    public Vector3[] spawnPos;
+
+    public float timeBetweenObstacleSpawnings = 12f;
+
+    private int index = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("SpawnObstaclePatterns");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnObstaclePatterns()
+    {
+        index = Random.Range(0, obstacleArray.Length);
+
+        yield return new WaitForSeconds(1f);
+        Instantiate(obstacleArray[index], new Vector3(gameObject.transform.position.x, spawnPos[index].y, spawnPos[index].z), Quaternion.identity);
+        yield return new WaitForSeconds(timeBetweenObstacleSpawnings);
+        StartCoroutine("SpawnObstaclePatterns");
     }
 }
