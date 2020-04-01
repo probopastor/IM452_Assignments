b86a4aef76b28f0fe43ff1778b4987cbcdca1265
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-31 21:03:46 -0500

    State methods set up

---
+++ nomikos_IM452_Assignment9/Assets/Scripts/BurnState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnState : MonoBehaviour, IEnemyState
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChasePlayer()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnFire()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Stunned()
+    {
+        throw new System.NotImplementedException();
+    }
+}
