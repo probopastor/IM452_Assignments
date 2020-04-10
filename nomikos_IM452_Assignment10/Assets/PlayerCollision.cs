ec7513cd0d9aa1a98c74d58981127d1b03617c75
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-09 21:32:24 -0500

    D

---
+++ nomikos_IM452_Assignment10/Assets/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
